aea50f728035104fe5920ac5e63edd0ec48eb043
rcslbaa <ifasmarani3125@gmail.com>
2024-06-04 18:33:16 +0700

    menambahkan int main

---
+++ exceptionStandard/exceptionStandard.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <exception>
+//untuk obyek exception yang akan digunakan
+#include <array>
+//untuk obyek array yang akan kita gunakan
+using namespace std;
+
+int main()
+{
+	cout << "Awal Program" << endl; //penanda 1:..
+	

a3b88a2b8a658f9b57482501bbf5fe4c95278352
rcslbaa <ifasmarani3125@gmail.com>
2024-06-04 18:34:20 +0700

    menambahkan try

--- exceptionStandard/exceptionStandard.cpp
+++ exceptionStandard/exceptionStandard.cpp
@@ -8,4 +8,10 @@ using namespace std;
 int main()
 {
 	cout << "Awal Program" << endl; //penanda 1:..
+	try {
+		array<int, 3> data = { 6, 7, 8 };
+		//pesan array integer 3 elemen
+		cout << data.at(5) << endl;
+		//memanggil array elemen ke 5
+	}
 	

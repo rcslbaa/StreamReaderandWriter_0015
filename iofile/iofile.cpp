ba05f9526ea4a89f2b2e83224b3cbe8d05f1e734
rcslbaa <ifasmarani3125@gmail.com>
2024-06-04 17:55:27 +0700

    membuka file dlm mode membaca dan menunjuk ke sebuah file

--- iofile/iofile.cpp
+++ iofile/iofile.cpp
@@ -25,3 +25,8 @@ int main() {
 	}
 	//selesai dalam menulis sekarang tutup filenya
 	outfile.close();
+
+	//membuka file dalam mode membaca
+	ifstream infile;
+	//menunjuk ke sebuah file
+	infile.open("contohfile.txt");

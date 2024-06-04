214fb06bf23d6a16b80d276377e096d88477a453
rcslbaa <ifasmarani3125@gmail.com>
2024-06-04 18:45:16 +0700

    menambahkan catch int main ...

--- manualexception/manualexception.cpp
+++ manualexception/manualexception.cpp
@@ -12,4 +12,9 @@ int main()
 		//blok ini akan dieksekusi
 		cout << "Pengecualian akan dieksekusi" << endl;
 	}
-	
+	catch (...) {
+		/*jika selain integer maka block ini akan dieksekusi*/
+		cout << "default Pengecualian dieksekusi" << endl;
+	}
+	return 0;
+}

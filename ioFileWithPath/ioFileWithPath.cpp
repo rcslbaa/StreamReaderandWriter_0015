ae94bb39511cfe03c24168eeb6cb8a18c313a67b
rcslbaa <ifasmarani3125@gmail.com>
2024-06-04 18:21:55 +0700

    menambahkan while, else, dan return

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -42,4 +42,16 @@ int main() {
 	{
 
 		// melakukan perulangan setiap baris
-		
+		while (getline(infile, baris))
+		{
+
+			//dan tampilkan di sini
+			cout << baris << '\n';
+		}
+		// tutup file tersebut setelah selesai
+		infile.close();
+	}
+	//jika tidak ditemukan file maka akan menampilkan ini
+	else cout << "Unable to open file";
+	return 0;
+}

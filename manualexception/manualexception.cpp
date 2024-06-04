d4395d1825f39422163fb6d1069dad58c7386563
rcslbaa <ifasmarani3125@gmail.com>
2024-06-04 18:44:53 +0700

    menambahkan catch int main

--- manualexception/manualexception.cpp
+++ manualexception/manualexception.cpp
@@ -8,4 +8,8 @@ int main()
 		//throw 0.5; //melemparkan sebuah integer maka
 		cout << "Pernyataan tidak akan dieksekusi" << endl;
 	}
+	catch (int a) {
+		//blok ini akan dieksekusi
+		cout << "Pengecualian akan dieksekusi" << endl;
+	}
 	

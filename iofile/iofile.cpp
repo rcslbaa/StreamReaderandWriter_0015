5d739bea3e2c27a8078c30fb037009b45fac19f9
rcslbaa <ifasmarani3125@gmail.com>
2024-06-04 18:17:39 +0700

    melakukan perulangan setiap baris, tampilkan isi, dan tutup file

--- iofile/iofile.cpp
+++ iofile/iofile.cpp
@@ -35,4 +35,13 @@ int main() {
 	//jika file ada maka
 	if (infile.is_open())
 	{
-		
+		//melakukan perulangan setiap baris
+		while (getline(infile, baris))
+		{
+			//dan tampilkan di sini
+			cout << baris << '\n';
+		}
+		//tutup file tersebut setelah selesai
+		infile.close();
+	}
+	
